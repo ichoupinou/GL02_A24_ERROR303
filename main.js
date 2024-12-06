280305ce70e7f13f4f1701c79d26228aeb94b994
Speedy-yjo <anaellle.melo@gmail.com>
2024-12-06 13:21:20 +0100

    Update main.js pour les modifications du organizeTokens

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Parser = require('./parser.js');
 const fs = require('fs');
 const path = require('path');
-const file = path.resolve(__dirname, './sample/edt.cru');
+const file = path.resolve(__dirname, 'sample/edttotal_clean.cru');
 let structuredData = null;
 
 // Suppression du warning Punycode causé par la SPEC-9
@@ -21,10 +21,9 @@ fs.readFile(file, 'utf8', async (err, data) => {
     // Créer une nouvelle instance de Parser
     const parser = new Parser();
 
-    console.log("Validation et tokenisation en cours...");
-
     try {
         // Parse et récupère les tokens structurés
+        console.log("Validation et tokenisation en cours...");
         const tokens = await parser.parseAndTokenize(data);
 
         if (tokens) {
@@ -56,6 +55,7 @@ fs.readFile(file, 'utf8', async (err, data) => {
  * tableau d'objets représentant des lignes de cours en données organisées
  * Exemple de structure : données organisées par cours avec leurs attributs
  *
+ * @function transformStructuredDataToClasses
  * @param {Array<string>} tokens - Les tokens à organiser.
  * @returns {Array<Object>} Données organisées.
  */
@@ -83,15 +83,16 @@ function transformStructuredDataToClasses(data) {
  * Organise les tokens en une structure de données.
  * Exemple de structure : Données organisées par cours avec leurs attributs
  *
+ * @function organizeTokens
  * @param {Array<string>} tokens - Les tokens à organiser.
  * @returns {Array<Object>} Données organisées.
  */
-function organizeTokensbis(tokens) {
+function organizeTokens(tokens) {
     const organized = [];
     let currentModule = null;
     let currentCourse = [];
     for (const token of tokens) {
-        if (/^\+[A-Z]{2,4}(\d{1,2})?[A-Z]?\d?$/.test(token)) {
+        if (/^\+[A-Z]{2,7}(\d{1,2})?[A-Z]?\d?$/.test(token)) {
             // Détection d'un nouveau module
             if (currentModule) {
                 // Si on a un cours partiellement rempli, on l'ajoute
@@ -108,7 +109,7 @@ function organizeTokensbis(tokens) {
             };
         } else if (currentModule) {
             // Ajouter le token au cours courant
-            if (/^1$|^(C|D|T)\d{1,2}$|^P=\d{1,3}$|^H=(L|MA|ME|J|V|S)$|^(\d|1\d|2[0-3]):[0-5]\d-(\d|1\d|2[0-3]):[0-5][0-9]$|^[A-Z]([0-9]|[A-Z])?$|^S=[A-Z]{1,3}\d{1,3}$/.test(token)) {
+            if (/^1$|^(C|D|T)\d{1,2}$|^P=\d{1,3}$|^H=(L|MA|ME|J|V|S)$|^(\d|1\d|2[0-3]):[0-5]\d-(\d|1\d|2[0-3]):[0-5][0-9]$|^[A-Z]([0-9]|[A-Z])?$|^S=(?:SPOR|[A-Z]{1,3}\d{1,3})?$/.test(token)) {
                 // Transformer les valeurs spécifiques
                 if (/^[CTD]\d{1,2}$/.test(token)) {
                     // Replace C1 -> CM1, T1 -> TP1, D1 -> TD1 dynamically
@@ -117,14 +118,14 @@ function organizeTokensbis(tokens) {
                         if (letter === 'C') return 'CM' + number;
                         if (letter === 'T') return 'TP' + number;
                         if (letter === 'D') return 'TD' + number;
-                        return match;  // If no match, return the original token
+                        return match;  // si pas de match trouvé, on return la valeur originale du token
                     });
-                    currentCourse.push(transformedToken);  // Add the transformed token
+                    currentCourse.push(transformedToken);  //ajout du transformedToken
                 } else if (/^P=\d{1,3}$/.test(token)) {
                     currentCourse.push(parseInt(token.split("=")[1])); // "P=50" -> 50 (int)
                 } else if (/^H=(L|MA|ME|J|V|S)$/.test(token)) {
                     currentCourse.push(token.split("=")[1]); // "H=MA" -> "MA"
-                } else if (/^S=[A-Z]{1,3}\d{1,3}|S=SPOR$/.test(token)) {
+                } else if (/^S=[A-Z]{1,3}\d{1,3}$/.test(token)) {
                     currentCourse.push(token.split("=")[1]); // "S=LAB102" -> "LAB102"
                 } else if (/^(\d|1\d|2[0-3]):[0-5]\d-(\d|1\d|2[0-3]):[0-5][0-9]$/.test(token)) {
                     const [start, end] = token.split("-"); // "08:00-09:30" -> ["08:00", "09:30"]
@@ -152,7 +153,7 @@ function organizeTokensbis(tokens) {
         organized.push(currentModule);
     }
 
-
+    //on range tout dans une structure de données
     structuredData = transformStructuredDataToClasses(organized)
 
     return structuredData;
