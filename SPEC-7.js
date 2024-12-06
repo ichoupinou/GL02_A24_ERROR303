9440b09f4beb0b9a45d39c483944d429f32aff92
Floskobendo <florent.perroux@gmail.com>
2024-12-06 14:27:05 +0100

    Rajout de commentaires

--- SPEC-7.js
+++ SPEC-7.js
@@ -1,4 +1,10 @@
 const SPEC_3 = require('./SPEC-3');
+/**
+ * Fonction qui affiche les salles et les créneaux où il y a un chevauchement.
+ *
+ * @param {string} data - Les données des cours
+ * @returns {void} Cette fonction ne retourne rien, mais affiche les salles et les créneaux où il y a un chevauchement.
+ */
 function checkOverlaps(data) {
     // Regroupe les classes par salle
     const classesByRoom = {};
@@ -34,7 +40,12 @@ function checkOverlaps(data) {
         console.log("Pas de chevauchement détecté.");
     }
 }
-
+/**
+ * Fonction qui vérifie si deux horaires de cours se superposent.
+ *
+ * @param {string, string, string, string} start1, end1, start2, end2 : heures de début et de fin des deux cours
+ * @returns {boolean} Renvoie vrai si les deux heures de cours se superposent, et faux sinon
+ */
 function areOverlapping(start1, end1, start2, end2) {
     const start1Time = SPEC_3.timeToMinutes(start1);
     const end1Time = SPEC_3.timeToMinutes(end1);
