a02adfea4576743ea328cb50dd8225a653425868
Speedy-yjo <anaellle.melo@gmail.com>
2024-12-06 11:12:26 +0100

    Petites modifications, version finale

--- SPEC-2.js
+++ SPEC-2.js
@@ -3,6 +3,27 @@ module.exports={verifSalle, printedMaxCapacity, MaxCapacity};
 const DataMain = require('./main.js');
 data = DataMain.structuredData
 
+// ------------------------------------------------------------
+// Fonction appelée depuis terminalcommande par la fonction RoomCapacity
+
+/**
+ * Fonction qui affiche la capacité maximale d'une salle donnée
+ * Exemple de donnée : "D105"
+ *
+ * @param {string} salle - La salle dont on veut afficher la capacité maximale.
+ * @returns {void} Cette fonction ne retourne rien, mais affiche la capacité maximale de la salle donnée
+ */
+function printedMaxCapacity(salle) {
+    if (!verifSalle(salle)){
+        console.log("Erreur : la salle n'existe pas dans la base de données.");
+        return;
+    }
+    console.log(`La capacité maximale de la salle ${salle} est : ${MaxCapacity(salle)}`);
+}
+
+// ------------------------------------------------------------
+// Fonctions appelée plusieurs fois depuis les autres fichiers
+
 /**
  * Fonction qui renvoie vrai si la salle donnée existe dans la base de donnée, et faux sinon
  * Exemple de donnée : "D105"
@@ -17,23 +38,8 @@ function verifSalle(salle) {
 }
 
 /**
- * Fonction qui affiche la capacité maximale d'une salle donnée
- * Exemple de donnée : "D105"
- *
- * @param {string} salle - La salle dont on veut vérifier l'existence dans la base de données.
- */
-function printedMaxCapacity(salle) {
-    let verif = verifSalle(salle); //peut partir
-    if (!verif){
-        console.log("Erreur : la salle n'existe pas dans la base de données.");
-        return;
-    }
-    console.log(`La capacité maximale de la salle ${salle} est : ${MaxCapacity(salle)}`);
-}
-
-/**
  * Fonction qui renvoie la capacité maximale d'une salle donnée
- * Exemple de donnée : "D105" (on sait que la salle existe dans la base de données)
+ * Exemple de donnée : "D105" (on sait déjà que la salle existe dans la base de données)
  *
  * @param {string} salle - La salle dont on veut avoir la capacité maximale.
  * @returns {int} La capacité maximale de la salle.
